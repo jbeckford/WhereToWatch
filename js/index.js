9e9646e51835132af6029d1a858a904c8c55c322
Jesus Beckford <jesus_beckford@beckfordinteractive.com>
2017-03-18 14:31:46 -0400

    WhereToWatch: Initial build up.

---
+++ js/index.js
@@ -0,0 +1,64 @@
+$(function() {
+  subscribeToSubmitEvent();
+
+  function subscribeToSubmitEvent(){
+    // Use $.on(submit) or $.click to figure out when the user clicks the "submit" button
+    $("#submit-btn").click(handleSubmit);
+  }
+
+  function handleSubmit(){
+    // Prevent a form submission using the event.preventDefault() functionUse the $.ready() handler to delay your code from executing until all DOM assets have been loaded
+    event.preventDefault();
+
+    var imageId = "Barry";
+    addImage(getNextImageId());    
+  }
+
+  function addImage(imageId){
+    getThumbNailContainer().append(getThumbNailElementHtml(imageId));
+  }
+
+  function getThumbNailContainer(){
+    return $("#thumbNailsContainer.row");
+  }
+
+  function getThumbNailElementHtmlByImageId(imageId){
+    return "<div class=\"col-lg-3 col-md-4 col-xs-6 thumb\">" +
+           "<a class=\"thumbnail\" href=\"#\">" + 
+           "<img class=\"img-responsive\" " +
+           "src=\"smiley.gif\" alt=\"\" " +
+           "</a>" +
+           "</div>";
+  }
+
+  function getThumbNailElementHtml(imageId){
+    return "<div class=\"col-lg-3 col-md-4 col-xs-6 thumb\">" +
+           "<a class=\"thumbnail\" href=\"#\">" + 
+           "<img class=\"img-responsive\" " + 
+           "src=\"" + 
+           getImagePathByImageId(imageId) + 
+           "\" alt=\"\"" +
+           // "style=\"width:400px;height:300px;border:0\"" + 
+           ">" + 
+           "</a>" +
+           "</div>";
+  }
+
+  var imageIndex = 0;
+  var imageIds = ["AbstractTheArtOfDesign", "AmandaKnox", "AmySchumerTheLeatherSpecial", "ArrestedDevelopment", "AudrieAndDaisy", "Barry", "BillBurrWalkYourWayOut", "BlackMirror", "BloodLine", "BuddyThindersTruck", "CallMeFrancis", "CedricTheEntertainerLiveFromTheVille", "ChefsTableFrance", "ChefsTableNewEpisodes", "ChewingGum", "Cooked", "DanaCarveyStraightWhiteMale60", "FourSeasonsInHavana", "FullerHouse", "GabrielIglesiasImSorryForWhatISaidWhenIWasHungry", "GilmoreGirlsAYearInTheLife", "GraceAndFrankie", "HipHopEvolution", "HouseOfCards", "DontFeelAtHomeAnymore", "JimGaffiganCinco", "KeithRichardsUnderTheInfluence", "LaNina", "LimenySticketsASeriesOfUnfortunateEvents", "Love", "LukeCage", "MakingAMurderer", "MarvelJessicaJones", "MasterOfNone", "MikrBirbigliaThankGodForJokes", "Narcos", "OneDayAtATime", "OrangeIsTheNewBlack", "SantaClaritaDiet", "StrangerThings", "TalesByLight", "Tallulah", "The13th", "TheCrown", "TheCubaLibreStory", "TheDoOver", "TheFundamentalsOfCaring", "TheOA", "TheRanch", "TheWhiteHelmets", "TokyoStories", "TonyRobbinsIAmNotYourGuru", "TrevorNoahAfraidOfTheDark", "UltimateBeastMaster", "UnbreakableKimmySchmidt", "WhatHappenedMissSimone", "WhiteRabbitProject", "WinterOnFireUkrainesFightForFreedom"];
+
+
+  function getImagePathByImageId(imageId){
+    return "images/moviesArt/" + imageId + ".jpg";
+  }
+
+  function getNextImageId(){
+    if(imageIndex >= imageIds.length){
+      imageIndex = 0;
+    }
+
+    var imageId = imageIds[imageIndex];
+    imageIndex = imageIndex + 1;
+    return imageId;
+  }
+});
