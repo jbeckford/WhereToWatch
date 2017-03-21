6cb73c010080b08f5ae283f968ccbf1aca495fc9
Jesus Beckford <jesus_beckford@beckfordinteractive.com>
2017-03-21 14:50:23 -0400

    Search string used.
    Does filtering as user types search term.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(function() {
   var thumbNailHtmlTemplate = detatchThumbNailHtmlTemplate();
-
+  subscribeToSearchTermChangedEvent();
   subscribeToSubmitEvent();
 
   function detatchThumbNailHtmlTemplate() {
@@ -13,15 +13,33 @@ $(function() {
     $(".thumb").remove();
   }
 
+  function subscribeToSearchTermChangedEvent(){
+    // Use $.on(submit) or $.click to figure out when the user clicks the "submit" button
+    $("#searchTerm").on("input", handleSearchTermChanged);
+  }
+
+  function handleSearchTermChanged(){
+    filterMovies();
+  }
+
   function subscribeToSubmitEvent(){
     // Use $.on(submit) or $.click to figure out when the user clicks the "submit" button
-    $("#submit-btn").click(handleSubmit);
+    $("#formSearchMovies").submit(handleSubmit);
   }
 
   function handleSubmit(){
     // Prevent a form submission using the event.preventDefault() functionUse the $.ready() handler to delay your code from executing until all DOM assets have been loaded
     event.preventDefault();
-    addImage2(getNextImageId());    
+    filterMovies();
+  }
+
+  function filterMovies() {
+    var searchTerm = getSearchTerm(); 
+    findAndDisplayMovies(searchTerm);
+  }
+
+  function getSearchTerm(){
+    return $("#searchTerm").val();
   }
 
   function addImage(imageId){
@@ -36,6 +54,65 @@ $(function() {
     return "#thumbNailsContainer";
   }
 
+  function findAndDisplayMovies(moviesSearchTerm){
+    var foundMovies = searchForMovies(moviesSearchTerm);
+    clearImages();
+    addImages(foundMovies);
+  }
+
+  function searchForMovies(moviesSearchTerm){
+    var foundMovies = [];
+
+    var pattern = new RegExp(moviesSearchTerm);
+
+    for(imageIdIndex = 0; imageIdIndex < imageIds.length; imageIdIndex++) {
+      if(pattern.test(imageIds[imageIdIndex])){
+        foundMovies.push(imageIds[imageIdIndex]);
+      }
+    }
+    return foundMovies;
+  }
+
+  function simulateSearch(moviesSearchTerm) {
+    var foundMovies = [];
+    startingIndex = Math.floor((Math.random() * imageIds.length)); 
+
+    for(i = 0; i < 10; i++){
+      foundMovies[i] = imageIds[(startingIndex + i) % imageIds.length];
+    }
+
+    return foundMovies;
+  }
+
+  function addImages(images){
+    for(imageIndex = 0; imageIndex < images.length; imageIndex++){
+      addImage2(images[imageIndex]);
+    }
+  }
+
+  function removeImage(imageId){
+    $(getImageIdSelector(imageId)).remove();
+  }
+
+  function getImageIdSelector(imageId){
+    return "#" + imageId;
+  }
+
+  function removeImages(images){
+    for(imageIndex = 0; imageIndex < images.length; imageIndex++){
+      removeImage(images[imageIndex]);
+    }
+  }
+
+  function clearImages() {
+    $(".thumb").remove();
+    // $(getImageClassSelector()).remove();
+  }
+
+  function getImageClassSelector(){
+    return ".whereToWatchImage";
+  }
+
   function addImage2(imageId){
     getThumbNailContainer2(imageId).appendTo(getThumbNailsContainerSelector());
   }
@@ -44,6 +121,7 @@ $(function() {
     thumbNailContainer = thumbNailHtmlTemplate.clone();
     thumbNailContainer.attr("id", "imageId");
     thumbNailContainer.children(0).children(0).attr("src", getImagePathByImageId(imageId));
+    thumbNailContainer.addClass(getImageClassSelector());
     return thumbNailContainer;
   }
 
